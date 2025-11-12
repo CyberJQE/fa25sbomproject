c409774844c6a226d35b530a7a1534b79e2d7d3a
CyberJQE <jcyber31@pm.me>
2025-11-12 15:36:44 -0800

    Updated Main.py file
    Added features including generate, analyze, clear, load, etc.

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
+from SBOMBuilder import generateSBOM
+from SBOMBuilder import clearSBOMs
+from SBOMBuilder import loadSBOMs
+from SBOMBuilder import saveSBOMs
+from SBOMBuilder import analyzeSBOMs
 import json
-from sklearn import datasets
-from sklearn.tree import DecisionTreeClassifier
+#todo: Add thorough documentation throughout the main.py and SBOMBuilder.py files.
 
+"""A CLI SBOM Application that provides the following capabilities:
+    - Generate an SBOM in SPDX format (https://spdx.github.io/spdx-spec/v2.3/relationships-between-SPDX-elements/).
+    - Load an SBOM. - Loads the previously generated SBOM.
+    - Save an SBOM. - Saves the generated SBOM
+    - Clear an SBOM. - Clears the generated SBOM.
+    - Analyze an SBOM. - Analyzes the generated SBOM and reports its components to the CLI.
+    - Exit the application.
+    
+.json is accepted as one of the file formats for SPDX SBOMs and is leveraged.
+The generated SBOM is contained in a dictionary which is appended to a list which is saved to a .json file.
+"""
 
-# The generateSBOM() method uses the dictionary data structure relying on user input to fulfill the requirements of a SBOM in SPDX format. Serialization is employed in which the file is written and opened. The dictionary is converted to json format which can be uploaded on FOSSA.
-#todo: Leveraging a source that can pull open source projects instead of relying on input would be the eventual goal.
+def main():
 
-def generateSBOM ():
+    while True:
+        print("Hello User, welcome to an SBOM Builder where you can build SBOMs in SPDX format.")
+        print("Please select one of the following options: \n 1. Generate an SBOM\n 2. Load a generated SBOM\n 3. Save a SBOM\n 4. Clear a SBOM\n 5. Analyze a SBOM\n 6. Exit the application.")
+        userInput = input("Select an option: ")
+        if userInput == "1": # Option 1 generates the SBOM and creates the SPDXsbom.json file.
+            SPDXsbom = generateSBOM() # Call the generateSBOM method from the SBOMBuilder.py file.
+            if not SPDXsbom: # If there are no SBOMs in the list the user should be informed.
+                print("There are no SBOMs.")
+                continue # if no SBOMs exist return to the menu.
+            try: # Try block that writes the list of SBOMs to a json file.
+                sbom_json = json.dumps(SPDXsbom, indent=4)
+                with open("SPDXsbom.json", "w", encoding="utf-8") as file:
+                    file.write(sbom_json)
+                print("SBOM generated successfully: SPDXsbom.json")
+            except Exception as e:
+                print("The SBOM was not generated.")
+        if userInput == "2": # Option 2 loads the saved SBOMs and prints the contents to the CLI.
+            loadSBOMs()
+            print("The SBOM(s) have been loaded.")
+        elif userInput == "3": # Option 3 saves the SBOMs and informs the user their SBOMs have been saved.
+            saveSBOMs(SPDXsbom)
+            print("SBOM saved successfully")
+        elif userInput == "4": # Option 4 clears the contents of the SBOMs as an empty list.
+            clearSBOMs()
+            print("SBOM cleared successfully")
+        elif userInput == "5": # Option 5 analyzes the SBOMs and reports their contents.
+            analyzeSBOMs(SPDXsbom)
+            print("The SBOM was analyzed successfully")
+        elif userInput == "6": # Option 6 closes the application.
+            print("Exiting the application.")
+            break
 
-    # todo: make the dictionary accessible to all methods in the main.py file.
-    sbomDictionary = {
-        "createdBy:": input(),
-        "createdDate:": input(),
-        "suppliedBy:": input(),
-        "name:": input(),
-        "packageVersion:": input(),
-        "packageURL:": input(),
-        "Relationship:": input()
-}
-    if "/" not in sbomDictionary["packageURL:"]:
-        print("You need to add a valid package URL.")
-    return(sbomDictionary)
-newSBOM = json.dumps(generateSBOM(), indent=4)
-with open("sbom.sbom", "w") as sbomFile:
-    sbomFile.write(newSBOM)
-SPDXsbom = generateSBOM()
-
-# todo: Load the saved SBOM to a file as depicted below.
-
-def loadSBOM():
-    with open("sbom.sbom", "r") as sbomFile:
-        sbomData = json.load(sbomFile)
-
-    return sbomData
-SPDXLoad = loadSBOM()
-
-#todo: Use Machine Learning for the analyzeSBOM method.
-def analyzeSBOM():
-    iris = datasets.load_iris()
-    x = iris.data
-    y = iris.target
-    clf = DecisionTreeClassifier()
-    clf.fit(x, y)
-    predictions = clf.predict(SPDXLoad)
-    print(predictions)
-
-    return SPDXLoad
-#todo: Improved this method in which it can delete a generated SBOM.
-def deleteSBOM():
-    with open("sbom.sbom", "r") as sbomFile:
-        sbomData = json.load(sbomFile)
-        for sbom in sbomData:
-            if sbomData[sbom] is not None:
-                del sbomData[sbom]
-    return (sbomData)
+if __name__ == "__main__":
+    main()
